98d3ed34b3898f95f241514a39b89cac0ce5dd90
DPL-Gamma <dpl-gamma@dpl-gammas-imac.domain.actdsltmp>
2014-01-27 13:48:55 -0700

    Initial commit before starting the project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMgr::Application.config.secret_token = '1548dcf3838ac135f5790215cf337e5f3a6d81cedeb6a77cee216d035cbe839904f6b5d667222df24e4526cdd144c52074788d5fdef46b03d6dd0020ee691ddb'
